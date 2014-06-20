7703c66080869b0b2cb54cee2e1bd9c772b21262
Flavio S. Glock <fglock@gmail.com>
2014-06-20 21:27:26 +0200

    Pitu - redefine function

--- pitu.c
+++ pitu.c
@@ -33,7 +33,7 @@ char my_program_2[] =
 "    return 0;\n"
 "}\n";
 char my_program_3[] =
-"int foo3(int n)\n"
+"int foo(int n)\n"
 "{\n"
 "    printf(\"Hello World! #2\\n\");\n"
 "    printf(\"fib(%d) = %d\\n\", n, fib(n));\n"
@@ -64,9 +64,48 @@ int main(int argc, char **argv)
     if (tcc_compile_string(s, my_program_2) == -1)
         return 1;
 
-    // tcc_undefine_symbol(s, "foo");
+    /* as a test, we add a symbol that the compiled program can use.
+       You may also open a dll with tcc_add_dll() and use symbols from that */
+    tcc_add_symbol(s, "add", add);
+
+    /* relocate the code */
+    if (tcc_relocate(s, TCC_RELOCATE_AUTO) < 0)
+        return 1;
+
+    /* get entry symbol */
+    func = tcc_get_symbol(s, "foo");
+    if (!func)
+        return 1;
+
+    /* run the code */
+    func(32);
+
+    /* delete the state */
+    tcc_delete(s);
+
+// start again
+
+
+    s = tcc_new();
+    if (!s) {
+        fprintf(stderr, "Could not create tcc state\n");
+        exit(1);
+    }
+
+    /* if tcclib.h and libtcc1.a are not installed, where can we find them */
+    if (argc == 2 && !memcmp(argv[1], "lib_path=",9))
+        tcc_set_lib_path(s, argv[1]+9);
+
+    /* MUST BE CALLED before any compilation */
+    tcc_set_output_type(s, TCC_OUTPUT_MEMORY);
+
+    if (tcc_compile_string(s, my_program_1) == -1)
+        return 1;
+    // if (tcc_compile_string(s, my_program_2) == -1)
+    //     return 1;
     if (tcc_compile_string(s, my_program_3) == -1)
        return 1;
+
 
     /* as a test, we add a symbol that the compiled program can use.
        You may also open a dll with tcc_add_dll() and use symbols from that */
@@ -86,6 +125,8 @@ int main(int argc, char **argv)
 
     /* delete the state */
     tcc_delete(s);
+
+
 
     return 0;
 }
